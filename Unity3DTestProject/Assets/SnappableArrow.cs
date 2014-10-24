d539fb93c898fd6e3acff79781ef8c5164299519
RiggyRob <riggy_rob92@hotmail.com>
2014-10-24 15:25:13 +0100

    Added initial SnappableBow and SnappableArrow scripts for the bow and arrow objects to snap together when a collision between them is detected

---
+++ Unity3DTestProject/Assets/SnappableArrow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnappableArrow : MonoBehaviour {
+	
+	// non-primitive type arrow object
+	public GameObject snappableArrow;
+	
+	// Use this for initialization
+	void Start () {
+		// primitive type arrow object
+		GameObject snappableArrowP = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		snappableArrowP.AddComponent<Rigidbody> ();
+		snappableArrowP.transform.position = new Vector3 (0, 0, 0);
+		snappableArrowP.transform.rotation = new Quaternion(0,90,0,0);
+		snappableArrowP.transform.localScale = new Vector3 (5.0f, 0.1f, 0.1f);
+
+		AddSnapNode (snappableArrow);
+	}
+	
+	void AddSnapNode(GameObject snappableArrow)
+	{
+		GameObject snapNode = new GameObject ();
+
+		// put the snapNode in the midpoint of the x and y axis, on the edge of the z axis of the arrow object
+		snapNode.transform.position =new Vector3(2.5f,0.05f,0.1f);
+
+		//snapNode.transform.position =new Vector3(snappableArrow.transform.position.x/2,
+		//                                         snappableArrow.transform.position.y/2,
+		//                                         snappableArrow.transform.position.z);
+
+		// Red node
+		snapNode.AddComponent<Renderer> ();
+		snapNode.renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Unity3DTestProject/Assets/SnappableBow.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SnappableBow : MonoBehaviour {
+	
+	// non-primitive type arrow object
+	public GameObject snappableBow;
+	
+	// Use this for initialization
+	void Start () {
+		// primitive type arrow object
+		GameObject snappableBowP = GameObject.CreatePrimitive (PrimitiveType.Cube);
+		snappableBowP.AddComponent<Rigidbody> ();
+		snappableBowP.transform.position = new Vector3 (2, 3, -3);
+		snappableBowP.transform.rotation = new Quaternion (0, 0, 0, 0);
+		snappableBowP.transform.localScale = new Vector3 (0.5f, 5f, 0.5f);
+
+		AddSnapNode (snappableBow);
+	}
+	
+	void AddSnapNode(GameObject snappableBow)
+	{
+		GameObject snapNode = new GameObject();
+
+		// put the snapNode in the midpoint of the x and y axis, on the edge of the z axis of the bow object
+		snapNode.transform.position =new Vector3(0.05f,2.5f,0f);
+
+		//snapNode.transform.position =new Vector3(snappableBow.transform.position.x/2,
+		//                                         snappableBow.transform.position.y/2,
+		//                                         snappableBow.transform.position.z);
+
+		// Red node
+		snapNode.AddComponent<Renderer> ();
+		snapNode.renderer.material.color = Color.red;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
